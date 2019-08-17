c7ed49844ac70748de3aabdf9ce12dc1b886e0b3
kishore <kishore@ceegees.in>
2019-08-17 13:43:04 +0530

    removed base url domain

--- src/config/env.js
+++ src/config/env.js
@@ -1,7 +1,7 @@
 module.exports = {
     HOST: {
-        DEV: 'https://ec2-54-184-79-143.us-west-2.compute.amazonaws.com',
-        PRODUCTION: 'https://ec2-54-184-79-143.us-west-2.compute.amazonaws.com',
+        DEV: 'ec2-54-184-79-143.us-west-2.compute.amazonaws.com',
+        PRODUCTION: 'ec2-54-184-79-143.us-west-2.compute.amazonaws.com',
     },
     AWS_ACCESS_KEY_ID: 'AKIAJVSGN3JMTELWT5RA',
     AWS_SECRET_ACCESS_KEY: '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',
