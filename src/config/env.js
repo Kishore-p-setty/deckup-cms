01cb9c39a9ed0e5c808d8bd2053e107671182890
kishore <kishore@ceegees.in>
2019-08-17 13:08:15 +0530

    added config

---
+++ src/config/env.js
@@ -0,0 +1,10 @@
+module.exports = {
+    HOST: {
+        DEV: 'ec2-54-184-79-143.us-west-2.compute.amazonaws.com',
+        PRODUCTION: 'ec2-54-184-79-143.us-west-2.compute.amazonaws.com',
+    },
+    AWS_ACCESS_KEY_ID: 'AKIAJVSGN3JMTELWT5RA',
+    AWS_SECRET_ACCESS_KEY: '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',
+    AWS_REGION: 'us-west-2',
+    AWS_BUCKET_NAME: 'deck-up-diy-videos'
+}
